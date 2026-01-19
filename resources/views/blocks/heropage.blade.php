41e1622e8452751571158123b1eddcf001638140
Jacob Frederiksen <jacobfred88@gmail.com>
2026-01-19 13:29:39 +0100

    make logo stand out

--- resources/views/blocks/heropage.blade.php
+++ resources/views/blocks/heropage.blade.php
@@ -3,6 +3,7 @@
         <div x-ref="media" class="absolute inset-0 w-full h-full scale-110 will-change-transform">
             <x-responsivemedia :mediagroup="$responsivemediagroup" sizes="1600px" />
         </div>
+        <div class="absolute inset-0 h-1/3 bg-gradient-to-b from-black/70 to-black/0"></div>
         @if (!$removegradiant)
             <div x-ref="gradient" class="absolute inset-0 bg-gradient-to-t from-red/70 to-red/0 opacity-0"></div>
         @endif
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -8,6 +8,7 @@
                 <div x-ref="media" class="absolute inset-0 w-full h-full scale-110 will-change-transform">
                     <x-media :mediagroup="get_field('page')['mediagroup']" sizes="1600px" />
                 </div>
+                <div class="absolute inset-0 h-1/3 bg-gradient-to-b from-black/70 to-black/0"></div>
                 {{-- <div x-ref="gradient" class="absolute inset-0 bg-gradient-to-t from-red/70 to-red/0 opacity-0"></div> --}}
             </div>
             <div class="container grid relative">
