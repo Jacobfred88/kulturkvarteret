5169f746237e4ae7a8b90548d255c10688fce98d
Jacob Frederiksen <jacobfred88@gmail.com>
2026-04-16 11:17:56 +0200

    make options for mobile video and desktop iamge

--- resources/views/components/responsivemedia.blade.php
+++ resources/views/components/responsivemedia.blade.php
@@ -24,13 +24,22 @@
 
     @if ($type === 'ambientvideo')
         @if ($desktopambientvideo)
-            <div class="absolute left-0 top-0 w-full h-full @if ($mobileambientvideo) hidden lg:block @endif">
+            <div class="absolute left-0 top-0 w-full h-full hidden lg:block">
                 <x-ambientvideo :src=$desktopambientvideo :poster="$desktopimage" />
             </div>
+        @elseif ($desktopimage)
+            <div class="absolute left-0 top-0 w-full h-full @if ($mobileimage) hidden lg:block @endif">
+                <x-image :image=$desktopimage :lazy="isset($lazy) ? $lazy : true" :sizes="isset($sizes) ? $sizes : null" />
+            </div>
         @endif
+
         @if ($mobileambientvideo)
-            <div class="absolute left-0 top-0 w-full h-full @if ($desktopambientvideo) lg:hidden @endif">
+            <div class="absolute left-0 top-0 w-full h-full lg:hidden">
                 <x-ambientvideo :src=$mobileambientvideo :poster="$desktopimage" />
+            </div>
+        @elseif ($mobileimage)
+            <div class="absolute left-0 top-0 w-full h-full @if ($desktopimage) lg:hidden @endif">
+                <x-image :image=$mobileimage :lazy="isset($lazy) ? $lazy : true" :sizes="isset($sizes) ? $sizes : null" />
             </div>
         @endif
     @endif
